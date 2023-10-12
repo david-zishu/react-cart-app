6564062d9050f6ba7f81ff3cb7760283cadeea9b
David Zishu <david.zishu6393@gmail.com>
2023-10-12 12:18:04 +0530

    styles classes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+
+import "./index.css";
 
 function App() {
   return (
